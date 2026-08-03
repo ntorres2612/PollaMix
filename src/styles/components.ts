8a80035bbe91f110f50be258d0fa89d4de4604cb
Torres Tech Solutions <torrestech@192.168.0.169>
2026-08-03 11:58:27 -0500

    Sprint 1.2 - Design System

---
+++ src/styles/components.ts
@@ -0,0 +1,26 @@
+export const buttonStyles = {
+
+    primary:`
+    bg-red-600
+    hover:bg-red-700
+    rounded-xl
+    px-6
+    py-3
+    font-bold
+    transition
+    duration-300
+    shadow-xl
+    `,
+
+    secondary:`
+    bg-slate-700
+    hover:bg-slate-600
+    rounded-xl
+    px-6
+    py-3
+    font-bold
+    transition
+    duration-300
+    `,
+
+};
