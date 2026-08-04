ae6e5cb0dae1910d2ffda08e1b45ae5abfbfdbdc
Torres Tech Solutions <torrestech@192.168.0.169>
2026-08-03 19:40:29 -0500

    Sprint 2.0 - Backend NestJS y Prisma

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(3001);
+
+  console.log('🚀 Backend ejecutándose en http://localhost:3001');
 }
+
 bootstrap();
---
+++ backend/src/users/users.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  findAll() {
+    return [
+      {
+        id: 1,
+        name: 'Administrador',
+        email: 'admin@pollamix.com',
+      },
+    ];
+  }
+}
